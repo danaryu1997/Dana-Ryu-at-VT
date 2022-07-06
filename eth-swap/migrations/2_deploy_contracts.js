ad6f663f2d13e20e83e2b8547e0b6fef569def53
Dana Ryu <68269249+danaryu1997@users.noreply.github.com>
2022-07-06 22:13:09 +0900

    added token.sol

--- eth-swap/migrations/2_deploy_contracts.js
+++ eth-swap/migrations/2_deploy_contracts.js
@@ -1,5 +1,13 @@
+const Token = artifacts.require("Token");
 const EthSwap = artifacts.require("EthSwap");
 
-module.exports = function(deployer) {
+module.exports = async function(deployer) {
+  deployer.deploy(Token);
+  const token = await Token.deployed()
+  
   deployer.deploy(EthSwap);
+  const ethSwap = await EthSwap.deployed()
+
+  // Transfer all tokens to Ethswap
+  await token.transfer(ethSwap.address, '1000000000000000000000000')
 };
