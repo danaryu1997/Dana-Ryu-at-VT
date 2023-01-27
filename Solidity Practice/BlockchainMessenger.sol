33ca7029516d8d2840ca145bf7018ecc0ce6abe6
Dana Ryu <68269249+danaryu1997@users.noreply.github.com>
2023-01-27 12:03:18 +0900

    Implement Blockchain Messenger
    1. Through the constructor make sure you write the address that deployed the contract to a variable
    2. Create a message-variable of the type string, that only the deployer-address can update
    3. Create a counter, to see how many times the variable was updated.

---
+++ Solidity Practice/BlockchainMessenger.sol
@@ -0,0 +1,20 @@
+pragma solidity 0.8.15;
+
+contract BlockchainMessenger {
+    uint  public changeCounter;
+
+    address public owner;
+
+    string public theMessage;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function updateTheMessage(string memory _newMessage) public {
+        if(msg.sender == owner) {
+            theMessage = _newMessage;
+            changeCounter++;
+        }
+    }
+}
